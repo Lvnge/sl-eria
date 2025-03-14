8da2e43f1f7a0185b397469e8b517470a69892fa
Lvnge <roy9096@hotmail.com>
2025-03-13 20:27:34 -0600

    Test transition duration navbar

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -42,7 +42,7 @@ const NavBar = () => {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
-                transition={{ duration: 0.1 }}
+                transition={{ duration: 0 }}
                 className="flex items-center justify-center"
               >
                 {isDark ? (
